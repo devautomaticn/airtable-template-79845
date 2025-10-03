83b878f8a56e10e699b2ae772e0875531cb53239
lealmasa <jjlealmasa@gmail.com>
2025-10-03 08:07:04 -0300

    add gh actions

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
 
- base: "/airtable-template-79845/",
 
   
   server: {
