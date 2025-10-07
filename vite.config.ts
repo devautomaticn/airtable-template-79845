39e9908d476724798de1287ad6080f84898f0458
lealmasa <jjlealmasa@gmail.com>
2025-10-07 07:46:29 -0300

    set base path

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
 
- base: "/airtable-template-79845/",
+ base: "/",
 
   
   server: {
